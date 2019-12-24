41aab79f704e48beb903731f2966d445a980c7f3
José Victor Zeller <josevictorzg@gmail.com>
2019-12-24 18:47:49 -0300

    chore: add 'NoMenuLayout' layout

---
+++ src/layouts/NoMenuLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar color="primary">
+        <q-toolbar-title>
+          Your App Name
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
